3d1d0d808b260f680bfa3b462fb6aa49114cda77
tarushkaistha <167986207+tarushkaistha@users.noreply.github.com>
2025-02-19 18:37:40 +0530

    tested image url in fetchAllMessages() payload.

--- app/src/main/java/com/example/myapplication/NewsFragment.kt
+++ app/src/main/java/com/example/myapplication/NewsFragment.kt
@@ -1,19 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.databinding.NewsFragmentBinding
+import com.moengage.inbox.core.MoEInboxHelper
+import com.moengage.inbox.ui.view.InboxActivity
+import kotlinx.coroutines.launch
 
 class NewsFragment : Fragment() {
 
     private lateinit var binding: NewsFragmentBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = NewsFragmentBinding.inflate(layoutInflater)
@@ -22,6 +27,23 @@ class NewsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.defMoeInbox.setOnClickListener {
+//            val intent = Intent(requireActivity(), InboxActivity::class.java)
+//            startActivity(intent)
+
+
+
+            lifecycleScope.launch {
+                val inboxData = MoEInboxHelper.getInstance().fetchAllMessages(requireActivity())
+                Log.d("Utils.moengage", "my inbox data : $inboxData")
+
+                val mediaContent = inboxData!!.inboxMessages[0].mediaContent
+                val image = mediaContent!!.url
+
+                Log.d("utils.image", "my image url: $image ")
+            }
+        }
     }
 
     override fun onResume() {
