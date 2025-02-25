e8b1d510a9cb905a088ba71097a7c97a81505c2e
tarushkaistha <167986207+tarushkaistha@users.noreply.github.com>
2025-02-25 15:54:54 +0530

    tracked custom event attribute with JSON object and JSON array.

--- app/src/main/java/com/example/myapplication/NewsFragment.kt
+++ app/src/main/java/com/example/myapplication/NewsFragment.kt
@@ -6,14 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.NewsFragmentBinding
+import com.google.gson.JsonArray
+import com.moengage.core.MoECoreHelper
+import com.moengage.core.Properties
+import com.moengage.core.analytics.MoEAnalyticsHelper
+import com.moengage.core.enableAdIdTracking
+import org.json.JSONArray
+import org.json.JSONObject
 
 class NewsFragment : Fragment() {
 
     private lateinit var binding: NewsFragmentBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = NewsFragmentBinding.inflate(layoutInflater)
@@ -22,6 +28,50 @@ class NewsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        enableAdIdTracking(requireActivity())
+
+        binding.loginBtn.setOnClickListener {
+            MoEAnalyticsHelper.setUniqueId(requireActivity(), "jaysean")
+        }
+
+        binding.logoutBtn.setOnClickListener {
+            MoECoreHelper.logoutUser(requireActivity())
+        }
+
+
+
+        binding.trackCustomEvent.setOnClickListener {
+
+            val mapOfCountry = hashMapOf<String, String>()
+//
+//            mapOfCountry["USA"] = "Charlotte"
+//            mapOfCountry["India"] = "Bangalore"
+//
+//            println("my map of country : $mapOfCountry")
+//
+//            val arrayMapOfCountry = arrayListOf(mapOfCountry)
+
+            val intArray = JSONArray()
+            intArray.put(1)
+            intArray.put(2)
+
+//            println("my array map of country : ${arrayMapOfCountry[0]}")
+
+            val obj = JSONObject()
+            obj.put("usa","charlotte")
+            obj.put("india","chandigarh")
+
+
+            val arrayOfCountry = JSONArray()
+            arrayOfCountry.put(obj)
+
+            val property = Properties()
+            property.addAttribute("country", arrayOfCountry.toString())
+            MoEAnalyticsHelper.trackEvent(requireActivity(), "Country_Event", property)
+        }
+
+
     }
 
     override fun onResume() {
